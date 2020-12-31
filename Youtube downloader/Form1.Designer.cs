a2315316fcb71e8c399674603d248f796c783fa2
Timo <kneifel73@gmail.com>
2020-12-31 20:09:35 +0100

    Init

---
+++ Youtube downloader/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Youtube_downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Youtube Url:";
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(459, 58);
+            this.DownloadButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(100, 28);
+            this.DownloadButton.TabIndex = 2;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(41, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Mp4";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 165);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Youtube downloader/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Windows.Forms;
+using VideoLibrary;
+using System.IO;
+
+
+namespace Youtube_downloader
+{
+    public partial class Form1 : Form
+    {
+        Progress<double> Progress = new Progress<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private async void DownloadButton_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic);
+            if (!string.IsNullOrEmpty(url))
+            {
+                var yt = YouTube.Default;
+                var video = await yt.GetVideoAsync(url);
+                string title;
+                if(checkBox1.Checked)
+                    title = video.Title + ".mp4";
+                else
+                    title = video.Title + ".mp3";
+                string fullPath = Path.Combine(path, title);
+                File.WriteAllBytes(fullPath, await video.GetBytesAsync());
+            }
+
+        }
+    }
+}
+
+        
+
+     
+
